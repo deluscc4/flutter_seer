b0ad7f815aa8f6bf3c0507414d1dae267f68845f
deluscc4 <delusccafs@icloud.com>
2024-02-13 15:16:54 -0300

    my studies

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,49 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
-    home: Container(),
+    home: VidenteVirtualPage(),
   ));
+}
+
+class VidenteVirtualPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xff332B25),
+      appBar: AppBar(
+        backgroundColor: Colors.brown[800],
+        title: Text('Vidente Virtual'),
+      ),
+      body: Vidente(),
+    );
+  }
+}
+
+class Vidente extends StatefulWidget {
+  @override
+  State<Vidente> createState() => _VidenteState();
+}
+
+class _VidenteState extends State<Vidente> {
+  int imageNumber = 1;
+
+  void mostraSorte() {
+    setState(() {
+      imageNumber = Random().nextInt(5) + 2;
+      ;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+          onPressed: () {
+            mostraSorte();
+          },
+          child: Image.asset('imagens/vidente$imageNumber.png')),
+    );
+  }
 }
